ac35f39638779308bca02478a4c752a9bfc923f6
변성재 <dfdo333@naver.com>
2021-01-27 16:39:49 +0900

    init

---
+++ src/main/java/com/studyhelper/StudyHelperEc2Application.java
@@ -0,0 +1,13 @@
+package com.studyhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyHelperEc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyHelperEc2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/studyhelper/StudyHelperEc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyHelperEc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

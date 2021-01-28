006fe39a0e9fbe4aa83becd1ff5c50f6fc63e082
변성재 <dfdo333@naver.com>
2021-01-28 16:25:00 +0900

    port change 8080 -> 8081 testing

--- src/main/java/com/studyhelper/test/HelloController.java
+++ src/main/java/com/studyhelper/test/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@GetMapping("/hello")
 	public String hello() {
-		return "안녕하세요";
+		return "안녕하세요 로로로로로";
 	}
 	@GetMapping("/hellow")
 	public String hellow() {
